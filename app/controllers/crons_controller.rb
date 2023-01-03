cf647004fa9eb5728e5691597b026c5612b60ff7
charlie <charlie@example.com>
2023-01-03 22:14:09 +0000

    see branch name

--- app/controllers/crons_controller.rb
+++ app/controllers/crons_controller.rb
@@ -19,11 +19,11 @@ class CronsController < ApplicationController
   end
     
   def start
-    user_id = "fillIn"
-    access_token = "fillIn"
-    date = Time.now.strftime("%Y/%m/%d") # date = YYYY/MM/DD
+    user_id = ""
+    access_token = ""
+    date = Time.now.strftime("%Y-%m-%d") # date = YYYY-MM-DD
     res = JSON.parse(RestClient.get("https://api.fitbit.com/1.2/user/#{user_id}/sleep/date/#{date}.json", headers={authorization: "Bearer #{access_token}"}))
-    p ('***RESPONSE:' + res)
+    p res
     if (res.key?(:meta)) # res should have a meta key when sleep logs are processing (requested during a sleep still ongoing) see "metaresponse" in https://dev.fitbit.com/build/reference/web-api/sleep/get-sleep-log-by-date/
       p "USER IS ASLEEP RIGHT NOW"
     end
